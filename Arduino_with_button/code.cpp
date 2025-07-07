3d3fa18c7e6cc816510fcd5d2543f9dd074dd142
Bhagirath <patelbhagirath736@gmail.com>
2025-07-07 17:56:32 +0530

    Added

--- Arduino_with_button/code.cpp
+++ Arduino_with_button/code.cpp
@@ -1,3 +1,4 @@
+// LED is set to ON when the button is pressed.
 const int BUTTON = 2;
 const int LED = 3;
 int BUTTONstate = 0;
@@ -18,5 +19,30 @@ void loop()
   else
   {
     digitalWrite(LED, LOW);
+  }
+}
+
+// LED is OFF when button is pressed (Opposite effect)
+
+const int BUTTON = 2;
+const int LED = 3;
+int BUTTONstate = 0;
+
+void setup()
+{
+  pinMode(BUTTON, INPUT);
+  pinMode(LED, OUTPUT);
+}
+
+void loop()
+{
+  BUTTONstate = digitalRead(BUTTON);
+  if (BUTTONstate == HIGH)
+  {
+    digitalWrite(LED, LOW);
+  }
+  else
+  {
+    digitalWrite(LED, HIGH);
   }
 }
